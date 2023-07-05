a3499fdec0d884afcae3af153e297b0c56369590
John Daffue <56412825+deadeagle63@users.noreply.github.com>
2023-07-05 17:45:22 +0100

    fix(changed to bn for stake/unstake): changed the stake/unstake function to accept a BN instead
    changed input to BN to prevent ourside from causing any potential issues down the line

--- examples/stake.ts
+++ examples/stake.ts
@@ -1,18 +1,18 @@
 import {rewards,wallet,connection} from "./index";
 import {Keypair, sendAndConfirmTransaction, Transaction} from "@solana/web3.js";
-import {Wallet} from "@project-serum/anchor";
+import {Wallet,BN} from "@project-serum/anchor";
 const secondaryWallet = new Wallet(Keypair.generate())
-
+const oneGOFX = new BN(1e9)
 const exampleStakeDefault = async ()=>{
     // stake 1 GOFX
-    const txn= await rewards.stake(1e9)
+    const txn= await rewards.stake(oneGOFX)
 
     const confirmationId = await sendAndConfirmTransaction(connection,new Transaction().add(txn),[wallet.payer],{commitment:'confirmed'})
     console.log("TXN: ",confirmationId)
 }
 const exampleStakeSecondary= async ()=>{
     // stake 1 GOFX
-    const txn= await rewards.stake(1e9,secondaryWallet.publicKey)
+    const txn= await rewards.stake(oneGOFX,secondaryWallet.publicKey)
 
     const confirmationId = await sendAndConfirmTransaction(connection,new Transaction().add(txn),[secondaryWallet.payer],{commitment:'confirmed'})
     console.log("TXN: ",confirmationId)
